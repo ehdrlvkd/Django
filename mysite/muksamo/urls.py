0b0db1f8d78c5f7c0ac93ea4234942e6ebd70d25
DK <ehdrlvkd@naver.com>
2023-01-13 20:05:09 +0900

    commit

--- mysite/muksamo/urls.py
+++ mysite/muksamo/urls.py
@@ -4,5 +4,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index),
+    path('search_map/',views.search_map,name='search_map'),
 ]
 
--- mysite/muksamo/views.py
+++ mysite/muksamo/views.py
@@ -4,4 +4,15 @@ from django.http import HttpResponse
 
 def index(request):
     return render(request,'index.html',{})
+
+def search_map(request):
+
+    if request.method == 'POST':
+        data = request.POST['search-place']
+        print(data)
+        context ={
+            'data':data
+        }
+
+    return render(request,'search_map.html',context)
 # Create your views here.
