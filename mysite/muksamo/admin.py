2383850a5b8f034a064f70f16692a05326258d79
DK <ehdrlvkd@naver.com>
2023-01-13 17:55:05 +0900

    general commit

--- mysite/muksamo/admin.py
+++ mysite/muksamo/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 from .models import Question
 
+
+#admin 페이지에 데이터 정보 추가를 위해선 여기에 admin.site.register(model명)
 admin.site.register(Question)
 
 # Register your models here.
--- mysite/muksamo/models.py
+++ mysite/muksamo/models.py
@@ -1,5 +1,8 @@
 from django.db import models
 
+#새로운 data를 추가하고 싶다면 class 데이터클래스(models.Model) 내부에 스키마 데이터 작성
+
+
 
 class Question(models.Model):
     subject = models.CharField(max_length=200)
